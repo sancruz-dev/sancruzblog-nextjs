35b72bfbe32038aac345838300983d2e792f9af1
Sancruz-dev <sancruz.dev@gmail.com>
2022-08-15 18:14:08 -0300

    update 1

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
 
+
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+
+      <img 
+        src="/img/diving.png"
+        alt='diving image'
+      />
+
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
